b596ee82744693bd071b04544ac5943e02cb7853
swatchai <fsvskkeng@yahoo.com>
2021-06-17 19:28:06 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+    response.end("Hello NodeJS World.\n");
 });
 server.listen( port, function () {
   console.log('Listening on port: %s', port );
